782340990ead1584ba97c082155f61dfd2e7b3ed
VukV <vuk.vukovic.2000@gmail.com>
2024-01-11 19:40:14 +0100

    wotking on standard template + bug fixes

--- src/transform/generator.py
+++ src/transform/generator.py
@@ -1,10 +1,15 @@
+import os
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from weasyprint import HTML
 
 
+output_dir = 'output'
+
+
 def generate_cv(cv_model):
     env = Environment(
-        loader=FileSystemLoader('templates'),
+        loader=FileSystemLoader('src/transform/templates'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
@@ -26,7 +31,8 @@ def generate_cv(cv_model):
 
 def generate_html(cv_model, template):
     html_content = template.render(model=cv_model)
-    output_file = 'output/cv.html'
+    output_file = os.path.join(output_dir, 'cv.html')
+    create_output_dir()
 
     with open(output_file, 'w', encoding='utf-8') as f:
         f.write(html_content)
@@ -36,7 +42,13 @@ def generate_html(cv_model, template):
 
 def generate_pdf(cv_model, template):
     html_content = template.render(model=cv_model)
-    output_file = 'output/cv.pdf'
+    output_file = os.path.join(output_dir, 'cv.pdf')
+    create_output_dir()
     HTML(string=html_content).write_pdf(output_file)
 
     return output_file
+
+
+def create_output_dir():
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
